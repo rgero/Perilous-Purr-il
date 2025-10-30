3db07e187aa9faf62d66a81b91275e642a045427
Roy Gero <roy.gero.ii@gmail.com>
2025-10-30 18:31:09 -0400

    Rotating the Model to face direction.
    I might want to rotate the entire CharacterBody3D

--- Perilous Purr-il/Scripts/Player.gd
+++ Perilous Purr-il/Scripts/Player.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var body: Node3D = $Body
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,6 +22,10 @@ func _physics_process(delta: float) -> void:
 		animation_player.play("Walking")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		# Face input direction
+		var target_rot_y = atan2(direction.x, direction.z)
+		body.rotation.y = lerp_angle(body.rotation.y, target_rot_y, delta * 10.0)
 	else:
 		animation_player.stop()
 		velocity.x = move_toward(velocity.x, 0, SPEED)
