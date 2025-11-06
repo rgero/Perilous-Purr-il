1f677eef955ae1e3fa0a66999402aba04a229146
Roy Gero <roy.gero.ii@gmail.com>
2025-11-05 23:02:26 -0500

    Got the temporary bench working and blocking the enemy's ability to see the cat.

--- Perilous Purr-il/Scripts/Enemy.gd
+++ Perilous Purr-il/Scripts/Enemy.gd
@@ -1,40 +1,47 @@
 extends CharacterBody3D
-
 class_name Enemy
 
-@export var speed = 5.
-@export var attackRange: float = 1.5
-@export var maxHealth: float = 100
+@export var speed: float = 5.0
+@export var maxHealth: float = 100.0
+@export var eye_height: float = 1.5  # height from which to cast vision rays
+@export var aggro_range: float = 10.0  # optional detection distance
 
 @onready var navAgent: NavigationAgent3D = $NavigationAgent3D
 @onready var animationPlayer: AnimationPlayer = $AnimationPlayer
-@onready var collisonShape: CollisionShape3D = $Area3D/CollisionShape3D
+@onready var detectionArea: Area3D = $Area3D
 
 var provoked := false
-var player: Node3D
-
+var player: Node3D = null
 var spaceState: PhysicsDirectSpaceState3D
 
 func _ready() -> void:
-
 	spaceState = get_world_3d().get_direct_space_state()
-	assert(spaceState != null, "Space State is fucked")
+	assert(spaceState != null, "Space State not found.")
+	
+	# Connect signals safely in case they aren’t already connected
+	if not detectionArea.body_entered.is_connected(_on_area_3d_body_entered):
+		detectionArea.body_entered.connect(_on_area_3d_body_entered)
+	if not detectionArea.body_exited.is_connected(_on_area_3d_body_exited):
+		detectionArea.body_exited.connect(_on_area_3d_body_exited)
 
 func _process(_delta: float) -> void:
 	if player:
-		navAgent.target_position = player.global_position;
-	
+		navAgent.target_position = player.global_position
+
 func _physics_process(delta: float) -> void:
-	#var distance = global_position.distance_to(player.global_position)
-	#if distance <= aggroRange:
-		#provoked = true
-	#else:
-		#provoked = false
-		#
-	ChasePlayer(delta)
+	if provoked and player:
+		if not DetermineCanSee(player):
+			provoked = false
+			player = null
+			if animationPlayer.is_playing():
+				animationPlayer.stop()
+			return
+		ChasePlayer(delta)
+	elif provoked:
+		provoked = false
 
 func ChasePlayer(delta: float) -> void:
-	if !provoked:
+	if not provoked:
 		if animationPlayer.is_playing():
 			animationPlayer.stop()
 		return
@@ -42,7 +49,8 @@ func ChasePlayer(delta: float) -> void:
 	animationPlayer.play("Walking")
 		
 	var nextPosition = navAgent.get_next_path_position()
-	# Add the gravity.
+
+	# Add gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
@@ -59,28 +67,38 @@ func ChasePlayer(delta: float) -> void:
 
 func LookAtTarget(direction: Vector3) -> void:
 	var adjustedDirection = direction
-	adjustedDirection.y = 0;
+	adjustedDirection.y = 0.0
 	
-	# look_at uses a global position.
 	var lookAtDirection = global_position + adjustedDirection
 	if global_position.distance_to(lookAtDirection) > 0.001:
 		look_at(lookAtDirection, Vector3.UP, true)
 
 func DetermineCanSee(body: Node3D) -> bool:
+	if not body or not is_instance_valid(body):
+		return false
+
 	var params = PhysicsRayQueryParameters3D.new()
-	params.from = collisonShape.global_position
-	params.to = body.global_position
+	params.from = global_position + Vector3.UP * eye_height
+	params.to = body.global_position + Vector3.UP * 1.0
+	params.exclude = [self]
+	params.collide_with_areas = false
+	params.collide_with_bodies = true
+
 	var raycastResult = spaceState.intersect_ray(params)
+
 	if raycastResult:
 		var hitObject = raycastResult["collider"]
 		return hitObject.is_in_group("Player")
+
 	return false
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if DetermineCanSee(body):
-		provoked = true
-		player = body
+	if body.is_in_group("Player"):
+		if DetermineCanSee(body):
+			provoked = true
+			player = body
 
-func _on_area_3d_body_exited(_body: Node3D) -> void:
-	provoked = false
-	player = null
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body == player:
+		provoked = false
+		player = null
--- Perilous Purr-il/Scripts/Player.gd
+++ Perilous Purr-il/Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 6
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @export var playerCamera: PackedScene
 @export var cameraOffset: Vector3
@@ -15,6 +15,9 @@ var cameraInstance: Node
 func _ready() -> void:
 	cameraInstance = playerCamera.instantiate()
 	get_tree().current_scene.add_child.call_deferred(cameraInstance)
+	call_deferred("_position_camera")
+	
+func _position_camera() -> void:
 	cameraInstance.global_position += cameraOffset
 	
 func _process(_delta: float) -> void:
@@ -31,29 +34,19 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("Left", "Right", "Up", "Down")
 	var direction := Vector3(input_dir.x, 0, input_dir.y).normalized()
 
+	var target_speed = SPEED
+	if Input.is_action_pressed("Sprint") and currentSprint < sprintTime:
+		target_speed *= speedIncrease
+
 	if direction != Vector3.ZERO:
 		animation_player.play("Walking")
-		
-		# Rotate the CharacterBody3D itself
 		var target_rot_y = atan2(direction.x, direction.z)
 		rotation.y = lerp_angle(rotation.y, target_rot_y, delta * 10.0)
-
-		# Movement should now use world space (not local transform)
-		var move_dir = Vector3(input_dir.x, 0, input_dir.y).normalized()
-		
-		velocity.x = move_dir.x * SPEED
-		velocity.z = move_dir.z * SPEED
-		
-		if Input.is_action_pressed("Sprint") and currentSprint < sprintTime:
-			velocity.x *= speedIncrease
-			velocity.z *= speedIncrease
-
+		velocity.x = direction.x * target_speed
+		velocity.z = direction.z * target_speed
 	else:
 		animation_player.stop()
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		
-	if not Input.is_action_pressed("Sprint"):
-		currentSprint = 0
 
 	move_and_slide()
